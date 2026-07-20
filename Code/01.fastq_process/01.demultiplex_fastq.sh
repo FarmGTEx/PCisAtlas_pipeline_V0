761e16b3893eb9aad53c76ebc0a046f5f6e82e3a
Saixian Zhang <zhangsaixian@163.com>
2026-07-20 15:46:13 +0800

    Update demultiplex script and demo data files

--- Code/01.fastq_process/01.demultiplex_fastq.sh
+++ Code/01.fastq_process/01.demultiplex_fastq.sh
@@ -7,12 +7,13 @@ FQ_R1=SAMPLE_R1.fastq.gz
 FQ_R2=SAMPLE_R2.fastq.gz
 
 # generate reference barcode files
-preprocess_barcodes.indel.pl -i ~/samples.tb -o $output -b I5 I7 T5 T7 -bf ~/refindex -hd1 2 -hd2 2
+preprocess_barcodes.indel.pl -i barcodes -o $output -b I5 I7 T5 T7 -bf ./ -hd1 2 -hd2 2 -s spike-in
 
 # demultiplexing
-01.get_fixed.fq.indel.pl \
+get_fixed.fq.indel.pl \
 	-i $FQ_I1 $FQ_I2 $FQ_R1 $FQ_R2 \
 	-b1 $output.demx.barcode.barcodes \
 	-b2 $output.demx.barcode.samples \
+	-s $output.demx.spikein.barcodes \
 	-t 1 \
 	-o $output
